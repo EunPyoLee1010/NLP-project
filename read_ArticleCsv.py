0d6508c4fdbdc20cdc693a7bc1be6e6584ed071d
Gonglee <a71825485@gmail.com>
2019-12-09 12:00:20 +0900

    기사 데이터가 담긴 CSV 파일을 읽고 조사를 걸러내는 작업을 수행합니다.
    morphs_not_josa는 커스텀 메소드입니다. 주석 처리된 morphs_not_josa 함수 정의를 konlpy.tag 속 Okt 클래스에 메소드로 붙여넣기 한 후 사용하세요.

--- read_ArticleCsv.py
+++ read_ArticleCsv.py
@@ -12,3 +12,11 @@ print(df[0:3]['content'])
 # morphs_not_josa는 커스텀 메소드입니다. commit 메세지를 참고하세요.
 df['Token_content'] = df['content'].apply(okt.morphs_not_josa)
 print(df[0:3]['Token_content'])
+
+'''
+    def morphs_not_josa(self, phrase, norm=False, stem=False):
+        """Parse phrase to morphemes."""
+
+        return [s for s, t in self.pos(phrase, norm=norm, stem=stem) if t != 'Josa']
+'''
+
